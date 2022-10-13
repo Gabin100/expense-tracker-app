6a9e5c41bb45b8ec17c67107d504d107022b58bc
Jean Gabin Ishimwe <ij.gabin123@gmail.com>
2022-10-13 16:37:23 +0200

    add header button & Making expense items tappable

---
+++ components/UI/IconButton.js
@@ -0,0 +1,28 @@
+import { View, StyleSheet, Pressable } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+function IconButton({ icon, size, color, onPress }) {
+  return (
+    <Pressable
+      onPress={onPress}
+      style={({ pressed }) => pressed && styles.pressed}
+    >
+      <View style={styles.buttonContainer}>
+        <Ionicons color={color} name={icon} size={size} />
+      </View>
+    </Pressable>
+  );
+}
+
+export default IconButton;
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    borderRadius: 24,
+    padding: 8,
+    margin: 2,
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
