7cd87b7146cccafdede4684d5561476f5d660689
Jean Gabin Ishimwe <ij.gabin123@gmail.com>
2022-10-14 08:35:11 +0200

    configuring the form input Elements

---
+++ components/ManageExpense/ExpenseForm.js
@@ -0,0 +1,37 @@
+import { View, Text, StyleSheet } from "react-native";
+
+import Input from "./Input";
+
+function ExpenseForm() {
+  function amountChangedHandler() {}
+  return (
+    <View>
+      <Input
+        label='Amount'
+        textInputConfig={{
+          keyboardType: "decimal-pad",
+          onChangeText: amountChangedHandler,
+        }}
+      />
+      <Input
+        label='Date'
+        textInputConfig={{
+          placeholder: "YYYY-MM-DD",
+          maxLength: 10,
+          minLength: 10,
+          onChangeText: () => {},
+        }}
+      />
+      <Input
+        label='Description'
+        textInputConfig={{
+          multiline: true,
+        }}
+      />
+    </View>
+  );
+}
+
+export default ExpenseForm;
+
+const styles = StyleSheet.create({});
